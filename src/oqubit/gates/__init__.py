bbbee16856df68b63c12b84b2c5a6af88efa0220
Priyam Ghosh <priyamghosh2009@outlook.com>
2026-09-09 18:14:48 +0530

    feat(gates): expose single, controlled, multi quantum gates

---
+++ src/oqubit/gates/__init__.py
@@ -0,0 +1,5 @@
+from ..core.operators import Operator,Gate
+from .single import X,Y,Z,H,S,T,SX
+from .controlled import CX,CY,CZ,CH,CS,CSX,CT
+from .multi import SWAP,ISWAP,SQRT_SWAP,CCX,CSWAP
+__all__=["X","Y","Z","H","S","T","SX","CX","CY","CZ","CH","CS","CSX","CT","SWAP","ISWAP","SQRT_SWAP","CCX","CSWAP"]
