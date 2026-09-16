4e290c562ce24f0940d68a5f8c2c246425faafff
Priyam Ghosh <priyamghosh2009@outlook.com>
2026-09-16 16:58:41 +0530

    feat(algorithms): Add Quantum Algorithms package initializer

---
+++ src/oqubit/algorithms/__init__.py
@@ -0,0 +1,5 @@
+from .deutsch import deutsch
+from .deutsch_joza import deutsch_jozsa
+from .bernstein_vazirani import bernstein_vazirani
+from .superdense_coding import encode,decode
+__all__=["deutsch","deutsch_jozsa","bernstein_vazirani","encode","decode",]
